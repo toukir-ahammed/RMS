4fed94e27fdf71af98b1b190298d7f4bcdb99421
Toukir Ahammed <toukirahammedsajol@gmail.com>
2018-04-17 17:56:44 +0600

    Add project files.

---
+++ RMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RMS.Startup))]
+namespace RMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
