3c515d400c7b5477f7567c679230692e1f26b9ee
Toukir Ahammed <toukirahammedsajol@gmail.com>
2018-05-22 00:52:20 +0600

    Result Module Added

---
+++ RMS/Controllers/ResultController.cs
@@ -0,0 +1,62 @@
+﻿using RMS.Models;
+using RMSDataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RMS.Controllers
+{
+    [AllowAnonymous]
+    public class ResultController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Result
+        public ActionResult Index()
+        {
+            //return View();
+            return RedirectToAction("CheckResult");
+        }
+
+        // GET: /Result/CheckResult
+        [AllowAnonymous]
+        public ActionResult CheckResult()
+        {
+            //ViewBag.ReturnUrl = returnUrl;
+            ViewBag.DepartmentID = new SelectList(db.Departments, "DepartmentID", "Name");
+            return View();
+        }
+
+        //
+        // POST: Result/CheckResult
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult CheckResult(ResultViewModel model, string returnUrl)
+        {
+            ViewBag.DepartmentID = new SelectList(db.Departments, "DepartmentID", "Name");
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            
+            
+            var student =  db.Students
+                .Where(c => (c.RegistrationNumber == model.RegistrationNumber))
+                .FirstOrDefault();
+
+            ViewBag.CGPA = 3.92;
+
+
+            if(student == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View("Result", student);
+        }
+    }
+}
---
+++ RMS/Models/ResultViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RMS.Models
+{
+    public class ResultViewModel
+    {
+        [Required]
+        [Display(Name = "Registration Number")]
+        public string RegistrationNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Department" )]
+        public int DepartmentId { get; set; }
+
+        [Required]
+        [Display(Name = "Semester/Year")]
+        public int SemesterOrYear { get; set; }
+    }
+}
